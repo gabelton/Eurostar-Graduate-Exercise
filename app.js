3fbc27a99f3faa126201490c27cf556293aff702
Gabe Naughton <gabriel.naughton135@gmail.com>
2019-10-13 11:18:07 +0100

    updated index.js to stop cors error

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import axios from 'axios'
 
 class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      todos: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3000/api/todos')
+      .then(res => this.setState({todos: res.data}))
+  }
   render() {
+    console.log(this.state)
     return (
       <h1>Hello World!</h1>
     )
--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const data = require("./data.json");
 
 const app = express();
 
+
+app.use(express.static('public'), function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+} );
+
 app.get("/api/todos", function (_req, res) {
   res.status(200).send(data);
 })
-app.use(express.static('public'));
 
 app.listen(3000, function () {
   console.log(`🚄 Server running @ http://localhost:3000`)
