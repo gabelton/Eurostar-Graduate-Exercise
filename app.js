fbd81d9b857559bd6b11ddeaa848574a64ec744d
Gabe Naughton <gabriel.naughton135@gmail.com>
2019-10-13 12:51:16 +0100

    got total number of completed

--- app.js
+++ app.js
@@ -13,15 +13,20 @@ class App extends React.Component {
 
   componentDidMount() {
     axios.get('http://localhost:3000/api/todos')
-      .then(res => this.setState({todos: res.data.sort((a,b) => (a.id < b.id) ? 1 : -1)}))
+      .then(res => this.setState({
+        todos: res.data.sort((a,b) => (a.id < b.id) ? 1 : -1),
+        completed: res.data.filter(todo => todo.completed)
+      }))
   }
   render() {
     console.log(this.state)
+    if (this.state.todos.length === 0) return <h1>Loading...</h1>
     return (
       <div>
         <h1>Hello World!</h1>
 
         {this.state.todos.map(todo => <li key={todo.id}>{todo.title}</li>)}
+        <p>{this.state.completed.length}</p>
       </div>
     )
   }
